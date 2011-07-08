fb8779f19efa282e9048406f657cae41acb2bfcc
Tilman Holschuh <tholschuh@pugpharm.com>
2011-07-08 14:28:59 -0700

    added some stuff to check rebar deps

--- compiler/erlang_check.erl
+++ compiler/erlang_check.erl
@@ -1,5 +1,19 @@
 #!/usr/bin/env escript
 
 main([Filename]) ->
-	compile:file(Filename, [basic_validation, report, {i, "include"},
-			{i, "../include"}, {d, 'TEST'}, {d, 'DEBUG'}]).
+    {ok, Cwd} = file:get_cwd(),
+    Deps = find_deps(Cwd),
+    code:add_paths(Deps),
+    Includes = get_includes(Deps), 
+%    io:format("~p~n", Includes),
+    compile:file(Filename, [basic_validation,  report,{i, "include"},
+            {i, "../include"}, {d, 'TEST'}, {d, 'DEBUG'}]++Includes).
+
+find_deps(Cwd) ->
+    case file:list_dir(Cwd++"/deps") of
+        {ok, Deps} -> [Cwd++"/deps/"++Dep || Dep <- Deps];
+        _ -> []
+    end.
+
+get_includes(Deps) ->
+    [{i, Dep++"/include"} || Dep <- Deps].
